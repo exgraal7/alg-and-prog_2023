26abd05217db92fbdbc60e4509210304d6796fe6
exgraal7 <144822299+exgraal7@users.noreply.github.com>
2023-09-30 22:33:03 +0300

    Add files via upload

---
+++ contest_01/3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b;
+    
+    cin >> a >> b;
+
+    double result = a / b;
+    cout << result << endl;
+    
+    return 0;
+}
