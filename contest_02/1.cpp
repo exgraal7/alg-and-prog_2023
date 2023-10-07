3b489d4a1f619b656102e7a134017811c7fc41ae
exgraal7 <144822299+exgraal7@users.noreply.github.com>
2023-10-07 23:06:34 +0300

    Create 1.cpp

---
+++ contest_02/1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+bool is_prime(int n) {
+    if (n < 2) {
+        return false;
+    }
+    for (int i = 2; i * i <= n; ++i) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+int main(){
+    int number;
+    std::cin >> number;
+    std::cout << (is_prime(number) ? "YES" : "NO") << std::endl;
+}
