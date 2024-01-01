76aea0c5e334df87250e8b5e52e8a255ac72fdaa
exgraal7 <144822299+exgraal7@users.noreply.github.com>
2024-01-01 20:26:59 +0300

    Create 10.go

---
+++ contest_08/10.go
@@ -0,0 +1,54 @@
+package main
+import "fmt"
+
+type Human struct{
+    name string
+    age int
+}
+
+// Начало вставленного кода
+func printTypes(data []interface{}) {
+    for _, val := range data {
+        switch val.(type) {
+        case int:
+            fmt.Print("int ")
+        case string:
+            fmt.Print("string ")
+        case Human:
+            fmt.Print("Human ")
+        case *Human:
+            fmt.Print("*Human ")
+        default:
+            fmt.Print("interface ")
+        }
+    }
+    fmt.Println()
+}
+// Конец вставленного кода
+
+func main() {
+    var count int
+    fmt.Scan(&count)
+    
+    var data []interface{} = make([]interface{}, count)
+    for i:=0; i<count; i++{
+        var t string
+        fmt.Scan(&t)
+        switch t {
+            case "int": 
+                var val int = 0
+                data[i] = interface{}(val)
+            case "string":
+                var val string = "0"
+                data[i] = interface{}(val)
+            case "Human":
+                var val Human = Human{}
+                data[i] = interface{}(val)
+            case "*Human":
+                var val *Human = &Human{}
+                data[i] = interface{}(val)
+        }
+    }
+    
+    printTypes(data)
+}
